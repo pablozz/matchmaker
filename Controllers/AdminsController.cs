eded907952e25812061d3a362352abd15c18a3a7
mxstrong <mantas.ptakauskas@gmail.com>
2019-10-16 14:25:59 +0300

    Created controllers for corresponding models

---
+++ Controllers/AdminsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Matchmaker.Models;
+
+namespace Matchmaker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminsController : ControllerBase
+    {
+        private readonly MatchmakerContext _context;
+
+        public AdminsController(MatchmakerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Admins
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Admin>>> GetAdmins()
+        {
+            return await _context.Admins.ToListAsync();
+        }
+
+        // GET: api/Admins/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Admin>> GetAdmin(int id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return admin;
+        }
+
+        // PUT: api/Admins/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAdmin(int id, Admin admin)
+        {
+            if (id != admin.AdminId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(admin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdminExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Admins
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Admin>> PostAdmin(Admin admin)
+        {
+            _context.Admins.Add(admin);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAdmin", new { id = admin.AdminId }, admin);
+        }
+
+        // DELETE: api/Admins/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Admin>> DeleteAdmin(int id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            _context.Admins.Remove(admin);
+            await _context.SaveChangesAsync();
+
+            return admin;
+        }
+
+        private bool AdminExists(int id)
+        {
+            return _context.Admins.Any(e => e.AdminId == id);
+        }
+    }
+}
---
+++ Controllers/PlaygroundsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Matchmaker.Models;
+
+namespace Matchmaker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaygroundsController : ControllerBase
+    {
+        private readonly MatchmakerContext _context;
+
+        public PlaygroundsController(MatchmakerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Playgrounds
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Playground>>> GetPlaygrounds()
+        {
+            return await _context.Playgrounds.ToListAsync();
+        }
+
+        // GET: api/Playgrounds/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Playground>> GetPlayground(int id)
+        {
+            var playground = await _context.Playgrounds.FindAsync(id);
+
+            if (playground == null)
+            {
+                return NotFound();
+            }
+
+            return playground;
+        }
+
+        // PUT: api/Playgrounds/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayground(int id, Playground playground)
+        {
+            if (id != playground.PlaygroundId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(playground).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlaygroundExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Playgrounds
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Playground>> PostPlayground(Playground playground)
+        {
+            _context.Playgrounds.Add(playground);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlayground", new { id = playground.PlaygroundId }, playground);
+        }
+
+        // DELETE: api/Playgrounds/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Playground>> DeletePlayground(int id)
+        {
+            var playground = await _context.Playgrounds.FindAsync(id);
+            if (playground == null)
+            {
+                return NotFound();
+            }
+
+            _context.Playgrounds.Remove(playground);
+            await _context.SaveChangesAsync();
+
+            return playground;
+        }
+
+        private bool PlaygroundExists(int id)
+        {
+            return _context.Playgrounds.Any(e => e.PlaygroundId == id);
+        }
+    }
+}
