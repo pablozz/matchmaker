3f365bf67c5981a38b901ce15b442850c4fd720e
PauliusZ15 <paulius.z15@gmail.com>
2019-10-31 11:30:24 +0200

    use a better practice to display activities by days

--- ClientApp/src/components/UserApp/ActivityCardsDisplay.tsx
+++ ClientApp/src/components/UserApp/ActivityCardsDisplay.tsx
@@ -7,43 +7,66 @@ interface ICardsDisplayProps {
   activities: IActivity[];
 }
 
-interface IDateState {
+interface IDate {
   year: number;
   month: number;
   day: number;
 }
 
+interface IDateState {
+  date: IDate;
+  isNew: boolean;
+}
+
 export const ActivityCardsDisplay: React.FC<ICardsDisplayProps> = props => {
   const activities: IActivity[] = props.activities;
-  let prevDateState: IDateState = { year: 0, month: 0, day: 0 };
+
+  let prevDate: IDate = { year: 0, month: 0, day: 0 };
+
+  const dates: IDate[] = activities
+    .map((item: IActivity) => {
+      const newDate: IDate = {
+        year: getYear(item.date),
+        month: getMonth(item.date),
+        day: getDay(item.date)
+      };
+      const isNew: boolean =
+        newDate.year !== prevDate.year ||
+        newDate.month !== prevDate.month ||
+        newDate.day !== prevDate.day;
+      prevDate = newDate;
+      return { date: newDate, isNew: isNew };
+    })
+    .filter((item: IDateState) => {
+      return item.isNew;
+    })
+    .map((item: IDateState) => {
+      return item.date;
+    });
 
   return (
     <Fragment>
-      {activities.map((item: IActivity, index: number) => {
-        const dateState: IDateState = {
-          year: getYear(item.date),
-          month: getMonth(item.date),
-          day: getDay(item.date)
-        };
-        const dayHeader =
-          dateState.year === prevDateState.year &&
-          dateState.month === prevDateState.month &&
-          dateState.day === prevDateState.day ? null : (
-            <h2>{getDateString(dateState.month, dateState.day)}</h2>
-          );
-        prevDateState = dateState;
+      {dates.map((date: IDate, index: number) => {
         return (
           <Fragment key={index}>
-            {dayHeader}
-            <ActivityCard
-              key={item.id}
-              date={item.date}
-              category={item.category}
-              playground={item.playground}
-              playerLevel={item.playerLevel}
-              gender={item.gender}
-              participantsIn={item.users + '/' + item.numberOfParticipants}
-            />
+            <h2>{getDateString(date.month, date.day)}</h2>
+            {activities.map((activity: IActivity) => {
+              return date.year === getYear(activity.date) &&
+                date.month === getMonth(activity.date) &&
+                date.day === getDay(activity.date) ? (
+                <ActivityCard
+                  key={activity.id}
+                  date={activity.date}
+                  category={activity.category}
+                  playground={activity.playground}
+                  playerLevel={activity.playerLevel}
+                  gender={activity.gender}
+                  participantsIn={
+                    activity.users + '/' + activity.numberOfParticipants
+                  }
+                />
+              ) : null;
+            })}
           </Fragment>
         );
       })}
