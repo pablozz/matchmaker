a7d1cd8d58c068f0a0a082caf91285dfcf544266
PauliusZ15 <paulius.z15@gmail.com>
2019-12-15 20:57:32 +0200

    fix redirection bug after registration and login

--- ClientApp/src/components/UserApp/SignUp/SuccesfulRedirectFromSignUp.tsx
+++ ClientApp/src/components/UserApp/SignUp/SuccesfulRedirectFromSignUp.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Typography, Container } from '@material-ui/core';
 import { LoginForm } from '../Login/LoginForm';
 import { makeStyles } from '@material-ui/core/styles';
+import { ROUTES } from '../../../constants/routes';
+import { useCookies } from 'react-cookie';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -13,15 +16,19 @@ const useStyles = makeStyles(theme => ({
 
 export const SuccesfulRedirectFromSignUp: React.FC = () => {
   const classes = useStyles();
+  const [cookies] = useCookies(['loginToken']);
   return (
-    <Container className={classes.container} maxWidth="sm">
-      <Typography component="h1" variant="h3">
-        Registracija sėkminga
-      </Typography>
-      <br />
-      <Container maxWidth="xs">
-        <LoginForm />
+    <Fragment>
+      {cookies.loginToken !== '' && <Redirect to={ROUTES.Main} />}
+      <Container className={classes.container} maxWidth="sm">
+        <Typography component="h1" variant="h3">
+          Registracija sėkminga
+        </Typography>
+        <br />
+        <Container maxWidth="xs">
+          <LoginForm />
+        </Container>
       </Container>
-    </Container>
+    </Fragment>
   );
 };
