693c3fc139fa92bc3103b8784fb055ad86fb9d26
Paulius <zaranka.paulius@gmail.com>
2020-01-14 16:38:06 +0200

    redesign email message

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -15,12 +15,12 @@ namespace Matchmaker.Services
             var from = new EmailAddress("noreply@sportmatchmaker.azurewebsites.net", "Sport Matchmaker");
             var subject = "Please activate your Sport Matchmaker account";
             var to = new EmailAddress(recipient.Email);
-            var plainTextContent = $"Sveiki {recipient.Name}," +
-                " norėdami užbaigti savo registraciją paspauskite nuorodą." +
+            var plainTextContent = $"Sveiki, {recipient.Name}," +
+                " Norėdami užbaigti savo registraciją paspauskite nuorodą." +
                 " Jeigu tai ne jūs arba nesiregistravote mūsų svetainėje ignoruokite šį laišką.";
-            var htmlContent = $"<html><body><h2 style=\"font-size: 36\">Sveiki {recipient.Name},</h2><br>" +
-                "<div style=\"font-size: 20;\">norėdami užbaigti savo registraciją " +
-                $"<a style=\"color: blue\" href=\"https://sportmatchmaker.azurewebsites.net/api/auth/activate/{token.Id}\">paspauskite čia</a> <br>" +
+            var htmlContent = $"<html><body><h2 style=\"font-size: 36\">Sveiki, {recipient.Name},</h2><br>" +
+                "<div style=\"font-size: 20;\">Norėdami užbaigti savo registraciją " +
+                $"<a style=\"color: blue\" href=\"https://sportmatchmaker.azurewebsites.net/api/auth/activate/{token.Id}\">paspauskite čia</a><br><br>" +
                 "Jeigu tai ne jūs arba nesiregistravote šioje mūsų svetainėje ignoruokite šį laišką.</div></body></html>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
