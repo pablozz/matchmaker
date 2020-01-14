2aa8916cc896a17e50a6d28bf7b3185fcdd809fa
Augustina <37456430+Naktis@users.noreply.github.com>
2020-01-14 13:54:27 +0200

    Add redirection from MyAccount to Main

--- ClientApp/src/components/UserApp/User/MyAccount/MyAccount.tsx
+++ ClientApp/src/components/UserApp/User/MyAccount/MyAccount.tsx
@@ -1,15 +1,17 @@
 import React, { Fragment } from 'react';
 import { useCookies } from 'react-cookie';
+import { Redirect } from 'react-router';
 import { Toolbar } from '../../Toolbar';
+import { UpdateEmail } from './UpdateEmail';
+import { UpdatePassword } from './UpdatePassword';
+import { ROUTES } from '../../../../constants/routes';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     Container,
     Grid,
     Typography,
     Paper
-  } from '@material-ui/core';
-import { UpdateEmail } from './UpdateEmail';
-import { UpdatePassword } from './UpdatePassword';
+} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -33,47 +35,54 @@ export const MyAccount: React.FC = () => {
     const [cookie] = useCookies(['user']);
     const classes = useStyles();
 
+    function isLoggedIn() {
+        if (cookie.user)
+            return true;
+        else return false;
+    }
+
     return (
         <Fragment>
-        <Toolbar title="Mano paskyra" />
+            { isLoggedIn() ? 
+            <div>
+                <Toolbar title="Mano paskyra" />
+                <Container maxWidth="sm">
+                    <Paper className={classes.paper}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Typography variant="h4" align="center">{cookie.user.name}</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={3}>
+                                <Typography align="right">El. paštas:</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={9}>
+                                    <Typography>{cookie.user.email}</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={3}>
+                                <Typography align="right">Lytis:</Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={9}>
+                                    <Typography>{cookie.user.gender}</Typography>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Container>
+                    
+                <Container maxWidth="sm">
+                    <Paper className={classes.paper}>
+                            <Typography gutterBottom variant="h6">Keisti el. pašto adresą</Typography>
+                            <UpdateEmail />                
+                    </Paper>
+                </Container>
 
-        <Container maxWidth="sm">
-        <Paper className={classes.paper}>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Typography variant="h4" align="center">{cookie.user.name}</Typography>
-                </Grid>
-
-                <Grid item xs={12} sm={3}>
-                    <Typography align="right">El. paštas:</Typography>
-                </Grid>
-                <Grid item xs={12} sm={9}>
-                        <Typography>{cookie.user.email}</Typography>
-                </Grid>
-
-                <Grid item xs={12} sm={3}>
-                    <Typography align="right">Lytis:</Typography>
-                </Grid>
-                <Grid item xs={12} sm={9}>
-                        <Typography>{cookie.user.gender}</Typography>
-                </Grid>
-            </Grid>
-        </Paper>
-        </Container>
-        
-        <Container maxWidth="sm">
-         <Paper className={classes.paper}>
-                 <Typography gutterBottom variant="h6">Keisti el. pašto adresą</Typography>
-                <UpdateEmail />                
-        </Paper>
-        </Container>
-
-        <Container maxWidth="sm">
-        <Paper className={classes.paper}>
-            <Typography gutterBottom variant="h6" >Keisti slaptažodį</Typography>
-                <UpdatePassword />
-        </Paper>
-        </Container>
+                <Container maxWidth="sm">
+                    <Paper className={classes.paper}>
+                        <Typography gutterBottom variant="h6" >Keisti slaptažodį</Typography>
+                            <UpdatePassword />
+                    </Paper>
+                </Container>
+            </div>
+            : <Redirect to={ROUTES.Main} /> }          
         </Fragment>
     );
 };
