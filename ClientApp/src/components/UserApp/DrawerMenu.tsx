817e60de7981f1b50a6a02a4a98539df6ef6ca05
Paulius Zaranka <p.zaranka@ba.lt>
2023-01-08 20:26:24 +0200

    refactor

--- ClientApp/src/components/UserApp/DrawerMenu.tsx
+++ ClientApp/src/components/UserApp/DrawerMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, Dispatch } from 'react';
+import React, { Fragment, Dispatch } from 'react';
 import { useCookies } from 'react-cookie';
 import {
   Drawer,
@@ -9,7 +9,7 @@ import {
   ListItemText,
   Typography
 } from '@material-ui/core';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@mdi/react';
 import {
@@ -36,6 +36,10 @@ const useStyles = makeStyles(theme => ({
   },
   list: {
     width: theme.spacing(30)
+  },
+  link: {
+    color: 'black',
+    textDecoration: 'none'
   }
 }));
 
@@ -47,16 +51,8 @@ interface IDrawerMenuProps {
 export const DrawerMenu: React.FC<IDrawerMenuProps> = props => {
   const [cookie, , removeCookie] = useCookies(['user']);
 
-  const [redirectToMain, setRedirectToMain] = useState<boolean>(false);
-  const [redirectToLogin, setRedirectToLogin] = useState<boolean>(false);
-  const [redirectToUserActivities, setRedirectToUserActivities] = useState<
-    boolean
-  >(false);
-  const [redirectToUserAccount, setRedirectToUserAccount] = useState<boolean>(
-    false
-  );
-
-  const userActivityDispatch: Dispatch<IUserRegisteredActivitiesAction> = useDispatch();
+  const userActivityDispatch: Dispatch<IUserRegisteredActivitiesAction> =
+    useDispatch();
   const activityDispatch: Dispatch<IActivitiesAction> = useDispatch();
 
   const classes = useStyles();
@@ -69,60 +65,67 @@ export const DrawerMenu: React.FC<IDrawerMenuProps> = props => {
 
   return (
     <Fragment>
-      {redirectToMain && <Redirect to={ROUTES.Main} />}
-      {redirectToLogin && <Redirect to={ROUTES.Login} />}
-      {redirectToUserActivities && <Redirect to={ROUTES.UserActivities} />}
-      {redirectToUserAccount && <Redirect to={ROUTES.UserAccount} />}
       <Drawer open={props.isOpen} onClose={props.onClose}>
         {cookie.user && (
           <div className={classes.userData}>
-            <Typography variant="h5">{cookie.user.name}</Typography>
+            <Typography variant="h5" component="h2">
+              {cookie.user.name}
+            </Typography>
             <Typography variant="subtitle2">{cookie.user.email}</Typography>
           </div>
         )}
         <Divider />
         <div className={classes.list}>
           <List>
-            <ListItem button onClick={() => setRedirectToMain(true)}>
-              <ListItemIcon>
-                <Icon title="Home" size={1} path={mdiHome} />
-              </ListItemIcon>
-              <ListItemText primary="Pagrindinis" />
-            </ListItem>
-            {cookie.user && (
-              <ListItem
-                button
-                onClick={() => setRedirectToUserActivities(true)}
-              >
+            <Link to={ROUTES.Main} className={classes.link}>
+              <ListItem button>
                 <ListItemIcon>
-                  <Icon title="Activities" size={1} path={mdiBasketball} />
+                  <Icon title="Home" size={1} path={mdiHome} />
                 </ListItemIcon>
-                <ListItemText primary="Mano veiklos" />
+                <ListItemText primary="Pagrindinis" />
               </ListItem>
+            </Link>
+            {cookie.user && (
+              <Link to={ROUTES.UserActivities} className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Icon title="Activities" size={1} path={mdiBasketball} />
+                  </ListItemIcon>
+                  <ListItemText primary="Mano veiklos" />
+                </ListItem>
+              </Link>
             )}
             {cookie.user && (
-              <ListItem button onClick={() => setRedirectToUserAccount(true)}>
-                <ListItemIcon>
-                  <Icon title="Account" size={1} path={mdiAccount} />
-                </ListItemIcon>
-                <ListItemText primary="Mano paskyra" />
-              </ListItem>
+              <Link to={ROUTES.UserAccount} className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Icon title="Account" size={1} path={mdiAccount} />
+                  </ListItemIcon>
+                  <ListItemText primary="Mano paskyra" />
+                </ListItem>
+              </Link>
             )}
             <Divider />
             {cookie.user ? (
-              <ListItem button onClick={() => handleLogOut()}>
+              <ListItem
+                button
+                onClick={() => handleLogOut()}
+                className={classes.link}
+              >
                 <ListItemIcon>
                   <Icon title="Logout" size={1} path={mdiLogout} />
                 </ListItemIcon>
                 <ListItemText primary="Atsijungti" />
               </ListItem>
             ) : (
-              <ListItem button onClick={() => setRedirectToLogin(true)}>
-                <ListItemIcon>
-                  <Icon title="Login" size={1} path={mdiLogin} />
-                </ListItemIcon>
-                <ListItemText primary="Prisijungti" />
-              </ListItem>
+              <Link to={ROUTES.Login} className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Icon title="Login" size={1} path={mdiLogin} />
+                  </ListItemIcon>
+                  <ListItemText primary="Prisijungti" />
+                </ListItem>
+              </Link>
             )}
           </List>
         </div>
